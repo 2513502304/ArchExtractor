308a543f58381b0a60802cce0c822a3685ade4e9
clelele <2513502304@qq.com>
2025-12-15 20:30:33 +0800

    chore: update .gitignore

--- src/archextractor/__init__.py
+++ src/archextractor/__init__.py
@@ -4,7 +4,6 @@ from .archextractor import ArchExtractor
 __author__ = "ChijiangZhai"
 __email__ = "chijiangzhai@gmail.com"
 __description__ = """A module for extracting archive files, which wraps the patoolib library, can recursively process nested compressed package files."""
-__version__ = "0.1.0"
 
 __all__ = [
     "ArchExtractor",
